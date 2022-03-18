b863d5f9709eddcf36532eac3d7deef2d2476662
Romain Hunault <romain.hunault@e.email>
2022-03-18 23:23:27 +0000

    Add App Lounge

--- Apps/Android.mk
+++ Apps/Android.mk
@@ -6,6 +6,12 @@ LOCAL_CERTIFICATE := platform
 
 ifeq ($(PLATFORM_SDK_VERSION), 25)
   LOCAL_SRC_FILES := app-release-unsigned-nougat.apk
+else ifeq ($(PLATFORM_SDK_VERSION), 27)
+  LOCAL_SRC_FILES := app-release-unsigned.apk
+else ifeq ($(RELEASE_TYPE), test)
+  LOCAL_SRC_FILES := app-debug.apk
+else ifeq ($(RELEASE_TYPE), dev)
+  LOCAL_SRC_FILES := app-releaseDev.apk
 else
   LOCAL_SRC_FILES := app-release-unsigned.apk
 endif
