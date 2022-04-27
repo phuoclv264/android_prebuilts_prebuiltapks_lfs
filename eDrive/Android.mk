ac89a9031d0dd6ea6d458f747d8d59df0baddb0f
TheScarastic <warabhishek@e.email>
2022-04-27 11:15:32 +0000

    prebuiltapks: update apps in bulk
    edrive - https://gitlab.e.foundation/e/os/eDrive/-/commit/083feb0432b4cf6971eecc2ad1e355fbca696b1c
    advancedPrivacy - https://gitlab.e.foundation/e/privacy-central/privacycentralapp/-/commit/e1d3be6e413501d1a1e4036c0b6634e072619ecd
    blisslauncher - https://gitlab.e.foundation/e/os/BlissLauncher/-/commit/b9ffcb858adfb38bac0298719fdf5ed4fbc8ac9c
    AccountManager - https://gitlab.e.foundation/e/os/AccountManager/-/commit/87c9a41106bc24a51fcbfbdb150c0340b37a40c5

--- eDrive/Android.mk
+++ eDrive/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := eDrive
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := foundation.e.drive.beta-1-build-o-22-04-25T0810-release-unsigned.apk
+LOCAL_SRC_FILES := foundation.e.drive.beta-1-build-o-22-04-27T0324-release-unsigned.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
