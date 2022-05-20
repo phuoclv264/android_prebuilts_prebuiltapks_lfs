14622cb7a75f8f94f2948c28b7bec2b669d03134
Nishith Khanna <nishithkhanna@e.email>
2022-05-20 19:26:10 +0530

    Update Message and Bliss Launcher
    https://gitlab.e.foundation/e/os/BlissLauncher/-/commit/6866ac55369d21569ff6e088a5289377217fb3ce
    https://gitlab.e.foundation/e/os/Message/-/commit/98a18bb491b9127aa8c057f4aaeaefe9f18caa7b

--- BlissLauncher/Android.mk
+++ BlissLauncher/Android.mk
@@ -10,11 +10,11 @@ $(if $(shell [ $(PLATFORM_SDK_VERSION) -ge 29 ] && echo "ok"), \
     $(eval BUILD_GREATER_THAN_PIE := false))
 
 ifeq ($(PLATFORM_SDK_VERSION), 28)
-  LOCAL_SRC_FILES := app-apiOreo-release.apk
+  LOCAL_SRC_FILES := BlissLauncher-1.4.0-apiOreo.apk
 else ifeq ($(PLATFORM_SDK_VERSION), 29)
-  LOCAL_SRC_FILES := app-apiQ-release.apk
+  LOCAL_SRC_FILES := BlissLauncher-1.4.0-apiQ.apk
 else
-  LOCAL_SRC_FILES := app-apiR-release.apk
+  LOCAL_SRC_FILES := BlissLauncher-1.4.0-apiR.apk
 endif
 
 LOCAL_MODULE_CLASS := APPS
