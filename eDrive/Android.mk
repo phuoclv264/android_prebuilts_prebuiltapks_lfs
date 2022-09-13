90426c86a1b2bdc17c311fe88e63f1e2de8a1653
vincent Bourgmayer <vincent.bourgmayer@e.email>
2022-09-13 16:39:43 +0200

    Update eDrive apk.
    - Fix 5373 issue about hidden files that are detected and synchronized from device side

--- eDrive/Android.mk
+++ eDrive/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := eDrive
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := eDrive-1.2.0-release-unsigned.apk
+LOCAL_SRC_FILES := eDrive-1.2.1-release-unsigned.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
