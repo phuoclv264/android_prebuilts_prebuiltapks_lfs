85060f8955d22d3e4300e40b32287d800c82601e
TheScarastic <warabhishek@e.email>
2022-10-05 13:03:29 +0000

    prebuiltapks: Unify pvc apps

--- PrivacyCentral/Android.mk
+++ PrivacyCentral/Android.mk
@@ -22,14 +22,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := PrivacyCentral
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
-ifeq ($(PLATFORM_SDK_VERSION), 29)
-LOCAL_SRC_FILES := Advanced_Privacy-1.2.0-e29-release.apk
-else ifeq ($(PLATFORM_SDK_VERSION), 30)
-LOCAL_SRC_FILES := Advanced_Privacy-1.2.0-e30-release.apk
-else
-LOCAL_SRC_FILES := Advanced_Privacy-1.2.0-e32-release.apk
-endif
-
+LOCAL_SRC_FILES := Advanced_Privacy-1.2.0-e-release.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_REQUIRED_MODULES := privapp-permissions-foundation.e.privacycentralapp.xml libpdnsd libtor
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
