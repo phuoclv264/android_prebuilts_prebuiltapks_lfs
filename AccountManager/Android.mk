f0079b1a3fd7d8e3775b56f94eac56c6e97823e7
Rohit <merothh@e.email>
2022-10-10 13:59:02 +0530

    prebuiltapks: Declare relevant LOCAL_OPTIONAL_USES_LIBRARIES
    error: mismatch in the <uses-library> tags between the build system and the manifest:
    	- required libraries in build system: []
    	                 vs. in the manifest: []
    	- optional libraries in build system: []
    	                 vs. in the manifest: [androidx.window.extensions, androidx.window.sidecar]
    	- tags in the manifest (prebuilts/prebuiltapks/AccountManager/app-standard-release-unsigned.apk):
    		uses-library-not-required:'androidx.window.extensions'		uses-library-not-required:'androidx.window.sidecar'
    
    error: mismatch in the <uses-library> tags between the build system and the manifest:
    	- required libraries in build system: [com.android.location.provider]
    	                 vs. in the manifest: [com.android.location.provider]
    	- optional libraries in build system: []
    	                 vs. in the manifest: [androidx.window.extensions, androidx.window.sidecar]
    	- tags in the manifest (prebuilts/prebuiltapks/GmsCore/play-services-core-withMapbox-withoutNearby-release-unsigned.apk):
    		uses-library:'com.android.location.provider'		uses-library-not-required:'androidx.window.extensions'		uses-library-not-required:'androidx.window.sidecar

--- AccountManager/Android.mk
+++ AccountManager/Android.mk
@@ -6,4 +6,8 @@ LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := app-standard-release-unsigned.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions \
+                                 androidx.window.sidecar
+endif
 include $(BUILD_PREBUILT)
