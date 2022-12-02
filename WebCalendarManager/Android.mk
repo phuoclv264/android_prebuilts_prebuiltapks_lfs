2f3ff2e7afcf5fe13b392bfc21e06cba2feed9f2
Rohit R Sekhar <merothh@e.email>
2022-12-02 09:10:10 +0000

    WebCalenderManager: Drop LOCAL_OPTIONAL_USES_LIBRARIES
    * Fixup previous commit!
    * Unbreaks build on ~s

--- WebCalendarManager/Android.mk
+++ WebCalendarManager/Android.mk
@@ -6,8 +6,4 @@ LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := WebCalendarManager-2.0.3-standard-release-unsigned.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
-LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions \
-                                 androidx.window.sidecar
-endif
 include $(BUILD_PREBUILT)
