1affc87249b39b8170d0c5a51d8f062fdc6b50f4
althafvly <althafvly@gmail.com>
2022-12-05 10:33:38 +0530

    prebuiltsapks: Browser: add Trichrome

--- Browser/Android.mk
+++ Browser/Android.mk
@@ -1,13 +1,54 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Browser
-LOCAL_CERTIFICATE := platform
-LOCAL_MULTILIB := both
-LOCAL_OVERRIDES_PACKAGES := Jelly
-LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
-src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-LOCAL_SRC_FILES := ChromePublic_$(src_arch).apk
 LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_PRODUCT_MODULE := true
+LOCAL_MULTILIB := both
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
+my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 29),)
+LOCAL_REQUIRED_MODULES := \
+        TrichromeLibrary
+LOCAL_SRC_FILES := $(my_src_arch)/TrichromeChrome.apk
+else
+LOCAL_SRC_FILES := $(my_src_arch)/ChromePublic.apk
+endif
+LOCAL_OVERRIDES_PACKAGES := Jelly Browser2 QuickSearchBox
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := TrichromeLibrary
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_MULTILIB := both
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
+my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
+LOCAL_SRC_FILES := $(my_src_arch)/TrichromeLibrary.apk
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := BrowserWebView
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_MULTILIB := both
+LOCAL_CERTIFICATE := platform
+LOCAL_REQUIRED_MODULES := \
+        libwebviewchromium_loader \
+        libwebviewchromium_plat_support
+LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
+my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 29),)
+LOCAL_REQUIRED_MODULES += \
+        TrichromeLibrary
+LOCAL_SRC_FILES := $(my_src_arch)/TrichromeWebView.apk
+else
+LOCAL_SRC_FILES := $(my_src_arch)/SystemWebView.apk
+endif
+LOCAL_OVERRIDES_PACKAGES := webview
+LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
