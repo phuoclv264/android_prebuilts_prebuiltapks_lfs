c830732763306b14a44049a830fd01b85390a6d7
Alexandre Roux <alexandre.roux.danzi@lostpod.me>
2023-01-09 15:31:37 +0000

    Revert "prebuiltsapks: Browser: add Trichrome for 1.8-beta"
    This reverts commit 4015b74005b0d56a8ebc294a9e145041247ea854

--- BrowserWebView/Android.mk
+++ BrowserWebView/Android.mk
@@ -1,13 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := Browser
-LOCAL_CERTIFICATE := platform
+LOCAL_MODULE := BrowserWebView
+LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
 LOCAL_MULTILIB := both
-LOCAL_OVERRIDES_PACKAGES := Jelly
+LOCAL_OVERRIDES_PACKAGES := webview
+LOCAL_REQUIRED_MODULES := \
+        libwebviewchromium_loader \
+        libwebviewchromium_plat_support
 LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-LOCAL_SRC_FILES := ChromePublic_$(src_arch).apk
+LOCAL_SRC_FILES := SystemWebView_$(src_arch).apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
