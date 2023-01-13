818ded35e3e2c97dca24560d11d2c84692c1b157
Nishith Khanna <nishithkhanna@e.email>
2023-01-13 14:08:04 +0530

    Fix makefile for microg

--- GmsCore/Android.mk
+++ GmsCore/Android.mk
@@ -29,8 +29,8 @@ LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gms.xml
 LOCAL_REQUIRED_MODULES += microg.xml
 ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
 LOCAL_USES_LIBRARIES := com.android.location.provider
-LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions \
-                                 androidx.window.sidecar \
-                                 org.apache.http.legacy
+LOCAL_OPTIONAL_USES_LIBRARIES := org.apache.http.legacy \
+				 androidx.window.extensions \
+                                 androidx.window.sidecar
 endif
 include $(BUILD_PREBUILT)
