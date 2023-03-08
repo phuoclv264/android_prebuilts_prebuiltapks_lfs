492586ff6dcf0d23f42ebfc48dd689005f8f4b20
althafvly <althafvly@gmail.com>
2023-03-08 17:19:15 +0000

    TEST: Bromite v100.0.4896.57

--- Browser/Android.mk
+++ Browser/Android.mk
@@ -5,16 +5,16 @@ LOCAL_MODULE := Browser
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
 LOCAL_MULTILIB := both
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 29),)
-LOCAL_REQUIRED_MODULES := \
-        TrichromeLibrary
-LOCAL_SRC_FILES := $(my_src_arch)/TrichromeChrome.apk
-else
+#ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 29),)
+#LOCAL_REQUIRED_MODULES := \
+#        TrichromeLibrary
+#LOCAL_SRC_FILES := $(my_src_arch)/TrichromeChrome.apk
+#else
 LOCAL_SRC_FILES := $(my_src_arch)/ChromePublic.apk
-endif
+#endif
 LOCAL_OVERRIDES_PACKAGES := Jelly Browser2 QuickSearchBox
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
@@ -36,19 +36,19 @@ LOCAL_MODULE := BrowserWebView
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
 LOCAL_MULTILIB := both
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_loader \
         libwebviewchromium_plat_support
 LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 29),)
-LOCAL_REQUIRED_MODULES += \
-        TrichromeLibrary
-LOCAL_SRC_FILES := $(my_src_arch)/TrichromeWebView.apk
-else
+#ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 29),)
+#LOCAL_REQUIRED_MODULES += \
+#        TrichromeLibrary
+#LOCAL_SRC_FILES := $(my_src_arch)/TrichromeWebView.apk
+#else
 LOCAL_SRC_FILES := $(my_src_arch)/SystemWebView.apk
-endif
+#endif
 LOCAL_OVERRIDES_PACKAGES := webview
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
