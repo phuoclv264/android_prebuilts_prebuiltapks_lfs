81e806536bb020a2bc86f4ef023b2c0ab41f6d91
Fahim Salam Chowdhury <fahim@e.email>
2023-04-18 20:24:44 +0600

    [eDrive] update for 1.11-beta
    pipeline: https://gitlab.e.foundation/e/os/eDrive/-/pipelines/281129

--- eDrive/Android.mk
+++ eDrive/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := eDrive
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := eDrive-1.3.7-release-unsigned.apk
+LOCAL_SRC_FILES := eDrive-1.3.8-release-unsigned.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
