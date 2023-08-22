10cabbb1ef92590cfdd7742fa18c3d018328ae89
Fahim Salam Chowdhury <fahim@e.email>
2023-08-22 20:26:49 +0600

    [WebCalendarManager] update for 1.15-beta
    pipeline: https://gitlab.e.foundation/e/os/webcalendarmanager/-/pipelines/322707

--- WebCalendarManager/Android.mk
+++ WebCalendarManager/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := WebCalendarManager
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := WebCalendarManager-2.0.3-standard-release-unsigned.apk
+LOCAL_SRC_FILES := WebCalendarManager-2.2-beta.1-standard-release-unsigned.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
