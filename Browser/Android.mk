ea12a327f374d9aa1ee806d4ff73f2a3b58596ff
althafvly <althafvly@gmail.com>
2023-10-26 13:02:12 +0000

    prebuilts: Update Browser for v1.17-rc

--- Browser/Android.mk
+++ Browser/Android.mk
@@ -8,13 +8,9 @@ LOCAL_MULTILIB := both
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 29),)
 LOCAL_REQUIRED_MODULES := \
         TrichromeLibrary
 LOCAL_SRC_FILES := $(my_src_arch)/TrichromeChrome.apk
-else
-LOCAL_SRC_FILES := $(my_src_arch)/ChromePublic.apk
-endif
 LOCAL_OVERRIDES_PACKAGES := Jelly Browser2 QuickSearchBox
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
@@ -42,13 +38,9 @@ LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_plat_support
 LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 29),)
 LOCAL_REQUIRED_MODULES += \
         TrichromeLibrary
 LOCAL_SRC_FILES := $(my_src_arch)/TrichromeWebView.apk
-else
-LOCAL_SRC_FILES := $(my_src_arch)/SystemWebView.apk
-endif
 LOCAL_OVERRIDES_PACKAGES := webview
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
