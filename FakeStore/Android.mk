f6e0d54d7ad34fd56637e893bb80340fd84c1ba8
Jonathan Klee <jonathan.klee@e.email>
2023-12-20 09:07:51 +0100

    Add microG companion for a test session

--- FakeStore/Android.mk
+++ FakeStore/Android.mk
@@ -8,6 +8,8 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+USES_LIBRARY := androidx.window.extensions androidx.window.sidecar
+
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := FakeStore
@@ -17,5 +19,12 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := platform
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.android.vending.xml
+
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
+LOCAL_OPTIONAL_USES_LIBRARIES := $(USES_LIBRARY)
+else
+LOCAL_PROVIDES_USES_LIBRARY := $(USES_LIBRARY)
+endif
+
 include $(BUILD_PREBUILT)
 
