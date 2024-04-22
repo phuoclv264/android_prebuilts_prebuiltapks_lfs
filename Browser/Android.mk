03d48fb083b946295dd1e9e0a60176b54e7f0b5e
Nishith Khanna <nishithkhanna@e.email>
2024-04-22 14:56:09 +0530

    Browser: updata makefile for v2.0-beta.2

--- Browser/Android.mk
+++ Browser/Android.mk
@@ -15,6 +15,9 @@ my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
 LOCAL_REQUIRED_MODULES := \
         TrichromeLibrary
 LOCAL_SRC_FILES := $(my_src_arch)/TrichromeChrome.apk
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions
+endif
 LOCAL_OVERRIDES_PACKAGES := Jelly Browser2 QuickSearchBox
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
@@ -53,6 +56,9 @@ my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
 LOCAL_REQUIRED_MODULES += \
         TrichromeLibrary
 LOCAL_SRC_FILES := $(my_src_arch)/TrichromeWebView.apk
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions
+endif
 LOCAL_OVERRIDES_PACKAGES := webview
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
