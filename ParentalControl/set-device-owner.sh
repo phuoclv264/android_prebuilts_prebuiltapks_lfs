0e076e3babf5eaa6cfebf2e35bf631fe120c57c3
althafvly <althafvly@gmail.com>
2024-06-19 11:47:32 +0530

    prebuilts: Update parental control for 2.2-beta.2

--- ParentalControl/set-device-owner.sh
+++ ParentalControl/set-device-owner.sh
@@ -1,7 +1,5 @@
 #!/system/bin/sh
 
-if [ ! -e /data/system/device_owner_2.xml ]; then
-    dpm set-device-owner foundation.e.parentalcontrol/foundation.e.parentalcontrol.DeviceAdmin
-    # If there's multiple users, we won't be able to as device owner, so use profile owner.
-    dpm set-profile-owner foundation.e.parentalcontrol/foundation.e.parentalcontrol.DeviceAdmin
-fi
+dpm set-device-owner foundation.e.parentalcontrol/foundation.e.parentalcontrol.DeviceAdmin
+# If there's multiple users, we won't be able to as device owner, so use profile owner.
+dpm set-profile-owner foundation.e.parentalcontrol/foundation.e.parentalcontrol.DeviceAdmin
